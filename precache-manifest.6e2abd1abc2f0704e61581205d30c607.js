d142ee23b982a3e0c6b0e4cb379c66eb35f73627
Moritz Hartmann <moritz@thinksono.com>
2020-05-02 18:48:41 +0200

    Updates

---
+++ precache-manifest.6e2abd1abc2f0704e61581205d30c607.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38973c1d36db15a8ba9352c944026679",
+    "url": "/hosting/index.html"
+  },
+  {
+    "revision": "ac5328daf660a421d81e",
+    "url": "/hosting/static/css/main.47c28785.chunk.css"
+  },
+  {
+    "revision": "726766461582f92d1fe7",
+    "url": "/hosting/static/js/2.7e6c53c1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hosting/static/js/2.7e6c53c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac5328daf660a421d81e",
+    "url": "/hosting/static/js/main.bd9c32c1.chunk.js"
+  },
+  {
+    "revision": "83eaa7cb93d55dac1b11",
+    "url": "/hosting/static/js/runtime-main.29cc423a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hosting/static/media/logo.5d5d9eef.svg"
+  }
+]);
